0599ed32208efe27ccc8f581cb9cd94e1bfa41e3
Carlo <ferrerocarlo96@gmail.com>
2023-01-31 22:26:18 +0100

    FEAT: add ScrollToTop on route change

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -7,18 +7,21 @@ import Footer from "../../Components/Footer";
 import RedirectBanner from "../RedirectBanner";
 import Flea from "../Flea";
 import DecodeMorse from "../DecodeMorse";
+import ScrollToTop from "../../Components/ScrollToTop";
 
 const App = () => {
   return (
     <div style={{ backgroundColor: "#fafafa " }}>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/roman-numerals-decoder" element={<RomanConverter />} />
-        <Route path="/decode-morse" element={<DecodeMorse />} />
-        <Route path="/redirect-banner" element={<RedirectBanner />} />
-        <Route path="/flea" element={<Flea />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/roman-numerals-decoder" element={<RomanConverter />} />
+          <Route path="/decode-morse" element={<DecodeMorse />} />
+          <Route path="/redirect-banner" element={<RedirectBanner />} />
+          <Route path="/flea" element={<Flea />} />
+        </Routes>
+      </ScrollToTop>
       <Footer />
     </div>
   );
